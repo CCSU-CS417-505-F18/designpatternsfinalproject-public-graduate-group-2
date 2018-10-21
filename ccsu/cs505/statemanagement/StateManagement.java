992c53056589a9102f532de8fd852dacaede5c68
Dan <dan@sdcsol.com>
2018-10-21 09:41:00 -0400

    Made Store an interface, added unimplemented StateManagement class

---
+++ ccsu/cs505/statemanagement/StateManagement.java
@@ -0,0 +1,8 @@
+package ccsu.cs505.statemanagement;
+
+public class StateManagement implements StoreFactory {
+    public Store createStore() {
+        //not yet implemented
+        return null;
+    }    
+}
--- ccsu/cs505/statemanagement/Store.java
+++ ccsu/cs505/statemanagement/Store.java
@@ -1,9 +1,8 @@
 package ccsu.cs505.statemanagement;
 
-public abstract class Store {
-    public abstract <T> void addState(String stateName, T value);
-    public abstract <T> T getState(String stateName);
-    public abstract <T> void setState(String stateName, T value);
-    public abstract <T> void addSubscription(String stateName,
-            Subscriber subscriber);    
+public interface Store {
+    public <T> void addState(String stateName, T value);
+    public <T> T getState(String stateName);
+    public <T> void setState(String stateName, T value);
+    public <T> void addSubscription(String stateName, Subscriber subscriber);    
 }
